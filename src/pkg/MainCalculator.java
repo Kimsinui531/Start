36f14a601bc63852ad61a9d42d2a968c51980556
Kimsinui531 <rlatlsdml531@naver.com>
2024-10-14 16:37:15 +0900

    계산기 외형을 위한 기본 골격 생성

--- src/pkg/MainCalculator.java
+++ src/pkg/MainCalculator.java
@@ -1,4 +1,17 @@
 package pkg;
 
-public class MainCalculator {
+import javax.swing.*;
+
+public class MainCalculator extends JFrame {
+    MainCalculator(){
+        setTitle("계산기");
+        //코드 추가
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(250, 300);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+    new MainCalculator();
+    }
 }
