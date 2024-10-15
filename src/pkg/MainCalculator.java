5191d1081dfe21d37fb5f63577ebd69a95062f5f
Kimsinui531 <rlatlsdml531@naver.com>
2024-10-15 14:11:06 +0900

    계산기 외형을 위한 기본 골격 생성 입출력 값을 보여주는 창이 있는 패널과 숫자와 사칙 연산 버튼이 있는 패널 추가 계산기를 위한 입력 필드를 추가하고 스타일 및 속성 설정, 왼쪽 정렬 및 회색 배경을 가진 입력 필드 구현, 편집 불가능하도록 설정

--- src/pkg/MainCalculator.java
+++ src/pkg/MainCalculator.java
@@ -1,14 +1,14 @@
 package pkg;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainCalculator extends JFrame {
     private JTextField inputSpace; // 입력 필드
     private JTextArea area; // 히스토리 표시 영역
 
     MainCalculator(){
-        inputSpace = new JTextField();
-        area = new JTextArea();
+
 
         setTitle("계산기"); // 타이틀을 계산기로 지정
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창 닫기 클릭시 종료
@@ -18,10 +18,18 @@ public class MainCalculator extends JFrame {
         panelWindow(); // 입출력 값을 보여주는 창이 있는 패널
         panelButton(); // 숫자와 사칙 연산 버튼이 있는 패널
 
-
+        setVisible(true);
     }
 
     private void panelWindow() {
+        JPanel Panel = new JPanel();
+        inputSpace = new JTextField(15);
+        inputSpace.setEditable(false); // 편집 불가능하게 설정
+        inputSpace.setBackground(Color.LIGHT_GRAY); //배경색 그레이로 설정
+        inputSpace.setHorizontalAlignment(JTextField.LEFT);
+        inputSpace.setBounds(8, 55, 220, 50); // x:8 y:10 위치, 220*50 크기로 설정
+        Panel.add(inputSpace);
+        add(Panel);
     }
 
 
