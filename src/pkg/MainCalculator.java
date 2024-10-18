e1c0efa61d67cff231958b89f6cd5db322316df1
Kimsinui531 <rlatlsdml531@naver.com>
2024-10-18 21:35:58 +0900

    버튼 이름 추가 및 JButton 인스턴스 초기화
    - 글꼴을 "나눔고딕"으로 설정하고 볼드 스타일과 크기 증가
    - 기본 버튼 배경을 연한 회색으로 설정
    - 특정 버튼에 대한 배경 색상 사용자 지정:
      - "C" 버튼: 연한 핑크색
      - 연산자 버튼 (÷, +, ×, -): 연한 갈색
      - "=" 버튼: 빨간색

--- src/pkg/MainCalculator.java
+++ src/pkg/MainCalculator.java
@@ -41,46 +41,73 @@ public class MainCalculator extends JFrame {
     MainCalculator() {
         setTitle("계산기"); // 타이틀을 계산기로 지정
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창 닫기 클릭시 종료
-        setSize(250, 300);  // 계산기의 크기 지정
+        setSize(350, 520);  // 계산기의 크기 지정
 
+
+        panelExpression();
         panelWindow(); // 입출력 값을 보여주는 창이 있는 패널
         panelButton(); // 숫자와 사칙 연산 버튼이 있는 패널
 
         setVisible(true);
 
     }
+    private void panelExpression(){
+        JPanel ExpressionPanel = new JPanel();
+        ExpressionPanel.setLayout(new GridLayout());
+        inputSpace = new JTextField();
+        ExpressionPanel.add(inputSpace);
+        add(ExpressionPanel, BorderLayout.NORTH);
+    }
 
-    private void panelWindow() {
-        JPanel Panel = new JPanel();
-        Panel.setLayout(new BorderLayout());
+    private void panelWindow() {// 계산 결과값을 출력해주는 패널
+        JPanel WindowPanel = new JPanel();
+        WindowPanel.setLayout(new BorderLayout());
         inputSpace = new JTextField();
         inputSpace.setEditable(false); // 편집 불가능하게 설정
-        inputSpace.setBackground(Color.LIGHT_GRAY); //배경색 그레이로 설정
+        inputSpace.setBackground(new Color (40,40,40)); //배경색 그레이로 설정
         inputSpace.setHorizontalAlignment(JTextField.LEFT);
         inputSpace.setBounds(8, 55, 220, 50); // x:8 y:10 위치, 220*50 크기로 설정
-        Panel.add(inputSpace);
-        add(Panel);
+        inputSpace.setForeground(Color.white);
+        WindowPanel.add(inputSpace);
+        add(WindowPanel, BorderLayout.CENTER);
     }
-//gridBackLayout
-
 
     private void panelButton() {
 
         JPanel ButtonPanel = new JPanel();
-        ButtonPanel.setLayout(new GridLayout(4, 3, 10, 10));
-        ButtonPanel.setBounds(8, 115, 230, 235);
+        ButtonPanel.setLayout(new GridLayout(4,4));
 
         String[] button_names = {"7", "8", "9", "÷", "4", "5", "6", "×", "1", "2", "3", "-", "C", "0", "=", "+"}; //계산기 버튼 글자 배열
         JButton[] buttons = new JButton[button_names.length]; //버튼 배열
 
-        //        버튼 생성
+        //        버튼 생성 색 지정
         for (int i = 0; i < button_names.length; i++) {
             buttons[i] = new JButton(button_names[i]);
             buttons[i].setFont(new Font("나눔고딕", Font.BOLD, 20));
+            buttons[i].setBackground(Color.LIGHT_GRAY);
+            if (button_names[i].equals("C") ){
+                buttons[i].setBackground(new Color(255, 192, 203));
+            }
+            if (button_names[i].equals("÷") ){
+                buttons[i].setBackground(new Color(245, 222, 179));
+            }
+            else if (button_names[i].equals("+") ){
+                buttons[i].setBackground(new Color(245, 222, 179));
+            }
+            else if (button_names[i].equals("×") ){
+                buttons[i].setBackground(new Color(245, 222, 179));
+            }
+            else if (button_names[i].equals("-") ){
+                buttons[i].setBackground(new Color(245, 222, 179));
+            }
+            else if (button_names[i].equals("=") ){
+                buttons[i].setBackground(new Color (240, 110, 140));
+            }
+            buttons[i].setForeground(Color.WHITE);
             ButtonPanel.add(buttons[i]);
         }
 
-        add(ButtonPanel, BorderLayout.SOUTH);
+        add(ButtonPanel,BorderLayout.SOUTH);
 
     }
 
